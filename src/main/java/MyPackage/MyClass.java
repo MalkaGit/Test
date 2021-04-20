ae5edf8b893caa502316f7404ce6e2dcc263a654
regine <regine.issan@gmail.com>
2021-04-20 22:57:44 +0300

    1

--- src/main/java/MyPackage/MyClass.java
+++ src/main/java/MyPackage/MyClass.java
@@ -3,7 +3,7 @@ package MyPackage;
 public class MyClass {
     public static void main(String[] args) {
         System.out.println("Hello Java");
-        int i= 0;
+        int i= 100;
         i++;
     }
 }
